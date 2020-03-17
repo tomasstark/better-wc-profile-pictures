222098c8a9a13b7a12f4814ded086c6294b80af0
Tomas Stark <tom@tomstark.me>
2020-03-17 10:21:38 +0100

    Add main plugin file.

---
+++ better-wc-profile-pictures.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Plugin Name: Better WooCommerce Profile Pictures
+Plugin URI: https://github.com/tomasstark/better-wc-profile-pictures
+Description: Allows user to upload multiple profile pictures and choose primary one from already uploaded pictures.
+Version: 0.1
+Author: Tomas Stark
+Author URI: https://github.com/tomasstark
+License: GPL2
+*/
+
+namespace BWCPP;
+
+define( 'BWCPP_TEXT_DOMAIN', 'bwcpp' );
+define( 'BWCPP_VERSION', '0.1' );
+
+function activate() {
+	do_action( 'bwcpp_activate' );
+}
+
+register_activation_hook( __FILE__, '\BWCPP\activate' );
+?>
