08bd43fe83162abb5bf3ad757b0553508ed01577
Tomas Stark <tom@tomstark.me>
2020-03-18 12:41:56 +0100

    Ditch post_type logic as it's not needed for this project's scope.
    Dealing with attachments directly is much better approach that saves us
    extra queries.

--- inc/classes/class-main.php
+++ inc/classes/class-main.php
@@ -2,8 +2,6 @@
 namespace BWCPP;
 
 class Main {
-	public $post_type = 'bwcpp';
-
 	public function __construct() {
 		$this->hook();
 	}
@@ -16,29 +14,6 @@ class Main {
 		}
 
 		require_once( get_inc_dir() . '/classes/class-my-account.php' );
-
-		add_action( 'init', array( $this, 'register_post_type' ) );
-	}
-
-	public function register_post_type() {
-		$args = array(
-			'label'                 => __( 'Better WooCommerce Profile Picture', BWCPP_TEXT_DOMAIN ),
-			'supports'              => array( 'title', 'thumbnail' ),
-			'taxonomies'            => array(),
-			'hierarchical'          => false,
-			'public'                => false,
-			'show_ui'               => false,
-			'show_in_menu'          => false,
-			'show_in_admin_bar'     => false,
-			'show_in_nav_menus'     => false,
-			'can_export'            => true,
-			'has_archive'           => false,
-			'exclude_from_search'   => true,
-			'publicly_queryable'    => false,
-			'capability_type'       => 'page',
-		);
-
-		register_post_type( $this->post_type, $args );
 	}
 
 	public function admin_notices() {
