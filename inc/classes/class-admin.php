b2ef0b4d95d939717b3183905e67222ee7d5eabb
Tomas Stark <tom@tomstark.me>
2020-03-18 13:18:19 +0100

    Add meta box to order edit page to display image at the time of order.
    - Added is also a link to user's profile to display their current image.

--- inc/classes/class-admin.php
+++ inc/classes/class-admin.php
@@ -4,6 +4,7 @@ namespace BWCPP;
 class Admin {
 	public function __construct() {
 		add_action( 'admin_notices', array( $this, 'admin_notices' ) );
+		add_action( 'add_meta_boxes', array( $this, 'add_order_meta_box' ) );
 	}
 
 	public function admin_notices() {
@@ -19,6 +20,38 @@ class Admin {
 					?>
 				</p>
 			</div>
+			<?php
+		}
+	}
+
+	public function add_order_meta_box() {
+		\add_meta_box(
+			'bwcpp_profile_picture_box',
+			__( 'Profile Picture', BWCPP_TEXT_DOMAIN ),
+			array( $this, 'render_order_meta_box' ),
+			'shop_order',
+			'side',
+			'default'
+		);
+	}
+
+	public function render_order_meta_box() {
+		global $post_id;
+
+		$order           = \wc_get_order( $post_id );
+		$profile_picture = \get_post_meta( $post_id, '_bwcpp_picture_id', true );
+
+		if ( ! empty( $profile_picture ) ) {
+			$picture = wp_get_attachment_image_src( $profile_picture );
+
+			?>
+			<img src="<?php echo $picture[0]; ?>" alt="" width="100%">
+
+			<a href="<?php echo get_edit_user_link( $order->get_user_id() ); ?>">View user profile</a>
+			<?php
+		} else {
+			?>
+			No image
 			<?php
 		}
 	}
