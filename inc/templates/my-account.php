908faddfa3489798b83cdac6c6155278d07140e5
Tomas Stark <tom@tomstark.me>
2020-03-18 16:40:39 +0100

    Clean up my-account template.

--- inc/templates/my-account.php
+++ inc/templates/my-account.php
@@ -16,7 +16,7 @@
 			<?php foreach ( $pictures as $picture ) : ?>
 				<label>
 					<input type="radio" name="primary_picture" value="<?php echo $picture['id']; ?>" <?php checked( $primary_picture, $picture['id'] ); ?>>
-					<img src="<?php echo $picture['url']; ?>">
+					<img src="<?php echo $picture['url']; ?>" alt="">
 				</label>
 			<?php endforeach; ?>
 		</div>
@@ -26,16 +26,15 @@
 		<legend><?php _e( 'Upload new pictures', BWCPP_TEXT_DOMAIN ); ?></legend>
 
 		<p class="woocommerce-form-row form-row form-row-wide">
-			<label for="wcpp_upload_profile_picture"><?php _e( 'Select pictures from your computer, then click "Save Changes"', BWCPP_TEXT_DOMAIN ); ?></label>
-			<input type="file" id="wcpp_upload_profile_picture" name="pictures[]" multiple accept=".jpg,.png,.jpeg">
+			<label for="bwcpp_upload_picture"><?php _e( 'Select pictures from your computer, then click "Save Changes"', BWCPP_TEXT_DOMAIN ); ?></label>
+			<input type="file" id="bwcpp_upload_picture" name="pictures[]" multiple accept=".jpg,.png,.jpeg">
 		</p>
 	</fieldset>
 
 	<?php wp_nonce_field( $this->save_pictures_action, $this->save_pictures_nonce ); ?>
-
 	<input type="hidden" name="action" value="<?php echo $this->save_pictures_action; ?>">
 
 	<p>
-		<input type="submit" class="woocommerce-Button button" name="save_account_details" value="<?php _e( 'Save Changes', BWCPP_TEXT_DOMAIN ); ?>">
+		<input type="submit" class="woocommerce-Button button" name="bwcpp_save_profile_pictures" value="<?php _e( 'Save Changes', BWCPP_TEXT_DOMAIN ); ?>">
 	</p>
 </form>
