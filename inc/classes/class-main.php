8149ee837990abcebf7b2847fdaab45b83e47237
Tomas Stark <tom@tomstark.me>
2020-03-18 13:00:53 +0100

    Add limit control option name. Require User_Pictures class.

--- inc/classes/class-main.php
+++ inc/classes/class-main.php
@@ -2,6 +2,8 @@
 namespace BWCPP;
 
 class Main {
+	public static $limit_pictures_option_name = 'bwcpp_max_pictures_per_user';
+
 	public function __construct() {
 		$this->hook();
 	}
@@ -13,6 +15,8 @@ class Main {
 			return;
 		}
 
+		require_once( get_inc_dir() . '/classes/class-user-pictures.php' );
+		require_once( get_inc_dir() . '/classes/class-pictures-controller.php' );
 		require_once( get_inc_dir() . '/classes/class-my-account.php' );
 	}
 
