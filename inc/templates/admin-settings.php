0feec0dba0ea1696515994ee01df1dd9f9b1af1f
Tomas Stark <tom@tomstark.me>
2020-03-18 13:21:31 +0100

    Add admin settings template.

---
+++ inc/templates/admin-settings.php
@@ -0,0 +1,10 @@
+<div class="wrap">
+	<h2><?php _e( 'Better WC Profile Pictures Settings', BWCPP_TEXT_DOMAIN ); ?></h2>
+
+	<form method="post" action="options.php">
+		<?php settings_fields( 'bwcpp_settings' ); ?>
+		<?php do_settings_sections( 'bwcpp_settings' ); ?>
+
+		<?php submit_button(); ?>
+	</form>
+</div>
