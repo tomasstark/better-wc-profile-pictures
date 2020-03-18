787a59052c666582ab45c0018035625706863941
Tomas Stark <tom@tomstark.me>
2020-03-18 16:38:42 +0100

    Simplify `get_unique_filename` by passing `$ext` parameter. Add comments to helpers.

--- inc/helpers.php
+++ inc/helpers.php
@@ -1,6 +1,17 @@
 <?php
+/**
+ * Set of helper functions used across code base.
+ *
+ * @package bwcpp
+ */
+
 namespace BWCPP\Helpers;
 
+/**
+ * Checks if WooCommerce is activated.
+ *
+ * @return boolean
+ */
 function is_woocommerce() {
 	if ( class_exists( 'woocommerce' ) ){
 		return true;
@@ -9,11 +20,16 @@ function is_woocommerce() {
 	return false;
 }
 
-function get_unique_filename( $path, $name ) {
+/**
+ * Generates UUID for uploaded file.
+ *
+ * @hooked callback `wp_unique_filename`
+ *
+ * @return string
+ */
+function get_unique_filename( $path, $name, $ext ) {
 	$user = \get_current_user();
-	$pathinfo = pathinfo( $name );
-
-	$name = \wp_generate_uuid4() . '.' . $pathinfo['extension'];
+	$name = \wp_generate_uuid4() . $ext;
 
 	return $name;
 }
