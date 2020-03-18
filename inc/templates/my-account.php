692ae964c37d73a2c233c3501055c3d118add5e5
Tomas Stark <tom@tomstark.me>
2020-03-18 13:07:34 +0100

    List user profile pictures in My Account section as radios.

--- inc/templates/my-account.php
+++ inc/templates/my-account.php
@@ -1,13 +1,24 @@
 <form class="woocommerce-EditAccountForm edit-account bwcpp-my-account" action="" method="post" enctype="multipart/form-data">
 	<h3><?php _e( 'Profile Pictures', BWCPP_TEXT_DOMAIN ); ?></h3>
 
+	<?php
+	$user_pictures   = new \BWCPP\User_Pictures();
+	$pictures        = $user_pictures->get_pictures();
+	$primary_picture = $user_pictures->get_primary();
+	?>
+
 	<fieldset>
 		<legend><?php _e( 'Choose your primary picture', BWCPP_TEXT_DOMAIN ); ?></legend>
 
 		<p><?php _e( 'Click to choose a picture, then click "Save Changes"', BWCPP_TEXT_DOMAIN ); ?></p>
 
 		<div class="bwcpp-pictures">
-
+			<?php foreach ( $pictures as $picture ) : ?>
+				<label>
+					<input type="radio" name="primary_picture" value="<?php echo $picture['id']; ?>" <?php checked( $primary_picture, $picture['id'] ); ?>>
+					<img src="<?php echo $picture['url']; ?>">
+				</label>
+			<?php endforeach; ?>
 		</div>
 	</fieldset>
 
