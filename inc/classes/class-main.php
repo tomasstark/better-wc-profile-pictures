ef5eab4379217065611965e9b2b8ed3fe11ab505
Tomas Stark <tom@tomstark.me>
2020-03-18 13:15:20 +0100

    Hook to `get_avatar` filter to display user's primary picture if there's one.

--- inc/classes/class-main.php
+++ inc/classes/class-main.php
@@ -15,9 +15,49 @@ class Main {
 			return;
 		}
 
+		add_filter( 'get_avatar', array( $this, 'get_avatar' ), 10, 5 );
+
 		require_once( get_inc_dir() . '/classes/class-user-pictures.php' );
 		require_once( get_inc_dir() . '/classes/class-pictures-controller.php' );
 		require_once( get_inc_dir() . '/classes/class-my-account.php' );
+	}
+
+	public function get_avatar( $avatar, $id_or_email, $size, $default, $alt ) {
+		$user = null;
+
+		if ( is_numeric( $id_or_email ) ) {
+			$user = \get_user_by( 'id', (int) $id_or_email );
+		} else {
+			$user = \get_user_by( 'email', (string) $id_or_email );
+		}
+
+		if ( ! $user ) {
+			return $avatar;
+		}
+
+		$user_pictures      = new User_Pictures( $user->ID );
+		$primary_picture_id = $user_pictures->get_primary();
+
+		if ( empty( $primary_picture_id ) ) {
+			return $avatar;
+		}
+
+		$primary_url = $user_pictures->get_primary_src();
+
+		if ( empty( $primary_url ) ) {
+			return $avatar;
+		}
+
+		$avatar = sprintf(
+			'<img src="%s" alt="%s" class="%s" height="%s" width="%s">',
+			$primary_url,
+			$alt,
+			"avatar avatar-{$size} photo",
+			$size,
+			$size
+		);
+
+		return $avatar;
 	}
 
 }
