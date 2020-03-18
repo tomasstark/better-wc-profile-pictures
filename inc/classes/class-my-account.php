07f7775619dd60512dff37eae4b2d07671cdf9a3
Tomas Stark <tom@tomstark.me>
2020-03-18 16:50:59 +0100

    Add doc blocks to My_Account class.

--- inc/classes/class-my-account.php
+++ inc/classes/class-my-account.php
@@ -1,10 +1,38 @@
 <?php
+/**
+ * My_Account class file.
+ *
+ * @package bwcpp
+ */
+
 namespace BWCPP;
 
+/**
+ * My_Account class for registering hooks related to WooCommerce's My Account section.
+ */
 class My_Account {
-	public $my_account_endpoint  = 'profile-pictures';
+	/**
+	 * Last part of URL for profile pictures section.
+	 *
+	 * URL example: yoursite.com/my-account/profile-pictures
+	 *
+	 * @var string URL slug.
+	 */
+	public $my_account_endpoint = 'profile-pictures';
+
+	/**
+	 * Action input value to compare against when handling form submit.
+	 *
+	 * @var string Action name.
+	 */
 	public $save_pictures_action = 'save_profile_pictures';
-	public $save_pictures_nonce  = 'save-profile-pictures-nonce';
+
+	/**
+	 * Profile Picture's form nonce.
+	 *
+	 * @var string Nonce.
+	 */
+	public $save_pictures_nonce = 'save-profile-pictures-nonce';
 
 	public function __construct() {
 		add_action( 'init', array( $this, 'add_endpoint' ) );
@@ -14,10 +42,24 @@ class My_Account {
 		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_styles' ) );
 	}
 
+	/**
+	 * Adds rewrite endpoint to WordPress.
+	 *
+	 * @hooked action `init`
+	 *
+	 * @return void
+	 */
 	public function add_endpoint() {
 		\add_rewrite_endpoint( $this->my_account_endpoint, EP_PAGES );
 	}
 
+	/**
+	 * Adds menu link to WooCommerce My Account menu.
+	 *
+	 * @hooked filter `woocommerce_account_menu_items`
+	 *
+	 * @return array Menu links.
+	 */
 	public function add_menu_link( $links ) {
 		$profile_pictures_link = array(
 			"{$this->my_account_endpoint}" => __( 'Profile Pictures', BWCPP_TEXT_DOMAIN ),
@@ -31,15 +73,35 @@ class My_Account {
 		return $links;
 	}
 
+	/**
+	 * Includes front end template for My Account's section page.
+	 *
+	 * @hooked action `woocommerce_account_profile-pictures_endpoint`
+	 *
+	 * @return void
+	 */
 	public function handle_endpoint() {
 		include( get_inc_dir() . '/templates/my-account.php' );
 	}
 
+	/**
+	 * Handles My Account's page form submit.
+	 *
+	 * @hooked action `template_redirect`
+	 *
+	 * @return void
+	 */
 	public function handle_form_submit() {
+		/**
+		 * Return early if no nonce is present.
+		 */
 		if ( ! isset( $_POST[$this->save_pictures_nonce] ) ) {
 			return;
 		}
 
+		/**
+		 * Return early if nonce verification failed.
+		 */
 		if ( ! \wp_verify_nonce( $_POST[$this->save_pictures_nonce], $this->save_pictures_action ) ) {
 			return;
 		}
@@ -48,24 +110,47 @@ class My_Account {
 		$primary_picture_id = (int) $_POST['primary_picture'];
 		$user_pictures      = new User_Pictures();
 
+		/**
+		 * If primary picture has been selected and submitted, set it as primary using
+		 * `set_primary` method on `User_Pictures` class instance.
+		 */
 		if ( ! empty( $primary_picture_id ) ) {
 			$user_pictures->set_primary( $primary_picture_id );
 		}
 
+		/**
+		 * If there were pictures added to file input, process upload.
+		 */
 		if ( ! empty( $picture_files['name'] ) ) {
 			$upload = Pictures_Controller::handle_upload( $picture_files, $user_pictures );
 		}
 
 		if ( \is_wp_error( $upload ) ) {
+			/**
+			 * If upload failed, propagate upload method's error and add WooCommerce error notice.
+			 */
 			\wc_add_notice( $upload->get_error_message(), 'error' );
 		} else {
+			/**
+			 * On success, show success notice.
+			 */
 			\wc_add_notice( __( 'Pictures saved successfully.', BWCP_TEXT_DOMAIN ) );
 		}
 
-		\wp_safe_redirect( \wc_get_page_permalink( 'myaccount' ) . '/profile-pictures' );
+		/**
+		 * Redirect back to our My Account section and exit.
+		 */
+		\wp_safe_redirect( \wc_get_page_permalink( 'myaccount' ) . '/' . $this->my_account_endpoint );
 		exit();
 	}
 
+	/**
+	 * Adds minimal CSS to My Account pages.
+	 *
+	 * @hooked action `wp_enqueue_scripts`
+	 *
+	 * @return void
+	 */
 	public function enqueue_styles() {
 		if ( ! \is_account_page() ) {
 			return;
